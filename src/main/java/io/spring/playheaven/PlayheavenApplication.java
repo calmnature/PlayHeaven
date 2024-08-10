d56afe12d29f824eb893cfc8ed994dec004ad794
BE-KDW <calmnature@naver.com>
2024-08-10 14:37:02 +0900

    init

---
+++ src/main/java/io/spring/playheaven/PlayheavenApplication.java
@@ -0,0 +1,13 @@
+package io.spring.playheaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayheavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayheavenApplication.class, args);
+    }
+
+}
