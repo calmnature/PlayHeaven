5ed9403097a6abb6007eca5eb22968493ee658da
BE-KDW <calmnature@naver.com>
2024-08-12 16:38:44 +0900

    스프링 시큐리티 임시 제외

--- src/main/java/io/spring/playheaven/PlayheavenApplication.java
+++ src/main/java/io/spring/playheaven/PlayheavenApplication.java
@@ -2,10 +2,10 @@ package io.spring.playheaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class PlayheavenApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(PlayheavenApplication.class, args);
     }
