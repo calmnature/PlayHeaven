0c8413b3fda7bb0e616e391bc8a3d9b24069886c
BE-KDW <calmnature@naver.com>
2024-08-14 03:32:01 +0900

    Feat : 게임 전체 조회

--- src/main/java/io/spring/playheaven/game/repository/GameRepository.java
+++ src/main/java/io/spring/playheaven/game/repository/GameRepository.java
@@ -1,10 +1,16 @@
 package io.spring.playheaven.game.repository;
 
 import io.spring.playheaven.game.entity.Game;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
     Game findByGameName(String gameName);
+
+    Page<Game> findAllBySaledIsTrue(Pageable pageable);
 }
+
+
