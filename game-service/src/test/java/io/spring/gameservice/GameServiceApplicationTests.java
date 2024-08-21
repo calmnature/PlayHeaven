914788f85f3fa41f039f37ca7479634eaa7058eb
BE-KDW <calmnature@naver.com>
2024-08-21 21:15:11 +0900

    chore : game-service init(MSA)

---
+++ game-service/src/test/java/io/spring/gameservice/GameServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.gameservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
