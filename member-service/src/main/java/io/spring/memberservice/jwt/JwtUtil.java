2c243f6e83e2b5a50b00c218836481de89c036c1
BE-KDW <calmnature@naver.com>
2024-08-28 01:17:21 +0900

    chore : Spring Security 및 JWT 추가

---
+++ member-service/src/main/java/io/spring/memberservice/jwt/JwtUtil.java
@@ -0,0 +1,60 @@
+package io.spring.memberservice.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.spring.memberservice.member.entity.Member;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    // Header KEY 값
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    // 사용자 권한 값의 KEY
+    public static final String AUTHORIZATION_KEY = "auth";
+    // Token 식별자
+    public static final String BEARER_PREFIX = "Bearer ";
+    // 토큰 만료시간(ms)
+    private final long TOKEN_TIME = 30 * 60 * 1000L; // 30분
+    // Base64 Encode 한 SecretKey
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    private Key key;
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    // 토큰 생성
+    public String createToken(Member member) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(member.getEmail()) // 사용자 Email
+                        .claim(AUTHORIZATION_KEY, member.getRole()) // 사용자 권한
+                        .claim("memberId", member.getMemberId()) // Member PK
+                        .claim("name", member.getName()) // 사용자 이름
+                        .claim("nickName", member.getNickname()) // 사용자 닉네임
+                        .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
+                        .setIssuedAt(date) // 발급일
+                        .signWith(key, signatureAlgorithm) // 암호화 알고리즘
+                        .compact();
+    }
+
+    // JWT Cookie 에 저장
+    public void addJwtToHeader(String token, HttpServletResponse res) {
+        // JWT 헤더에 추가
+        res.setHeader(AUTHORIZATION_HEADER, token);
+    }
+}
