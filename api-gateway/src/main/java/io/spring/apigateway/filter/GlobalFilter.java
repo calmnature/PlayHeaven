d23ac8016661bd6d0df68788f16415fa16c8e630
BE-KDW <calmnature@naver.com>
2024-08-28 17:50:56 +0900

    Fix : PatternMatcher를 사용하여 PathVariable 처리 가능

--- api-gateway/src/main/java/io/spring/apigateway/filter/GlobalFilter.java
+++ api-gateway/src/main/java/io/spring/apigateway/filter/GlobalFilter.java
@@ -16,6 +16,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -27,6 +28,7 @@ import java.util.List;
 @Slf4j(topic = "API Gateway-GlobalFilter")
 public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
     private final JwtUtil jwtUtil;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     public static class Config{}
 
@@ -42,13 +44,18 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
             HttpHeaders headers = request.getHeaders();
             String authorization = headers.getFirst("Authorization");
             List<String> whiteList = Arrays.asList(
+              "/member-service/api/member/email/auth/**",
               "/member-service/api/member/regist",
               "/member-service/api/member/login"
             );
 
             String path = request.getURI().getPath();
             log.info("Request Path = {}", path);
-            if(whiteList.contains(path)){
+
+            boolean isPathAllowed = whiteList.stream()
+                    .anyMatch(pattern -> antPathMatcher.match(pattern, path));
+
+            if(isPathAllowed){
                 log.info("Routing login or regist");
                 return chain.filter(exchange);
             }
